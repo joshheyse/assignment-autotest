4584ac938c8ccd41637d66e46f71afff97a6b43a
Josh Heyse <josh@heyse.us>
2024-06-15 16:29:44 -0500

    fixed test script

--- test/assignment5/sockettest.sh
+++ test/assignment5/sockettest.sh
@@ -1,37 +1,37 @@
-# !/bin/bash
+#!/bin/bash
 # Tester script for sockets using Netcat
 
-pushd `dirname $0`
+pushd $(dirname $0)
 target=localhost
 port=9000
-function printusage
-{
-	echo "Usage: $0 [-t target_ip] [-p port]"
-	echo "	Runs a socket test on the aesdsocket application at"
-	echo " 	target_ip and port specified by port"
-	echo "	target_ip defaults to ${target}" 
-	echo "	port defaults to ${port}" 
+
+function printusage {
+  echo "Usage: $0 [-t target_ip] [-p port]"
+  echo "Runs a socket test on the aesdsocket application at"
+  echo "  target_ip and port specified by port"
+  echo "  target_ip defaults to ${target}"
+  echo "  port defaults to ${port}"
 }
 
 while getopts "t:p:" opt; do
-	case ${opt} in
-		t )
-			target=$OPTARG
-			;;
-		p )
-			port=$OPTARG
-			;;
-		\? )
-			echo "Invalid option $OPTARG" 1>&2
-			printusage
-			exit 1
-			;;
-		: )
-			echo "Invalid option $OPTARG requires an argument" 1>&2
-			printusage
-			exit 1
-			;;
-	esac
+  case ${opt} in
+  t)
+    target=$OPTARG
+    ;;
+  p)
+    port=$OPTARG
+    ;;
+  \?)
+    echo "Invalid option $OPTARG" 1>&2
+    printusage
+    exit 1
+    ;;
+  :)
+    echo "Invalid option $OPTARG requires an argument" 1>&2
+    printusage
+    exit 1
+    ;;
+  esac
 done
 
 echo "Testing target ${target} on port ${port}"
@@ -41,34 +41,33 @@ echo "Testing target ${target} on port ${port}"
 # @param1 : The string to send
 # @param2 : The previous compare file
 # Returns if the test passes, exits with error if the test fails.
-function test_send_socket_string
-{
-	string=$1
-	prev_file=$2
-	new_file=$(mktemp)
-	expected_file=$(mktemp)
+function test_send_socket_string {
+  string=$1
+  prev_file=$2
+  new_file=$(mktemp)
+  expected_file=$(mktemp)
 
-	echo "sending string ${string} to ${target} on port ${port}"
-	echo ${string} | nc ${target} ${port} -w 1 > ${new_file}
-	cp ${prev_file} ${expected_file}
-	echo ${string} >> ${expected_file}
-	
-	diff ${expected_file} ${new_file} > /dev/null
-	if [ $? -ne 0 ]; then
-		echo "Differences found after sending ${string} to ${target} on port ${port}"
-		echo "Expected contents to match:"
-		cat ${expected_file}
-		echo "But found contents:"
-		cat ${new_file}
-		echo "With differences"
-		diff -u ${expected_file} ${new_file}
-		echo "Test complete with failure"
-		exit 1
-	else
-		cp ${expected_file} ${prev_file}
-		rm ${new_file}
-		rm ${expected_file}
-	fi
+  echo "sending string ${string} to ${target} on port ${port}"
+  echo ${string} | nc ${target} ${port} -w 1 >${new_file}
+  cp ${prev_file} ${expected_file}
+  echo ${string} >>${expected_file}
+
+  diff ${expected_file} ${new_file} >/dev/null
+  if [ $? -ne 0 ]; then
+    echo "Differences found after sending ${string} to ${target} on port ${port}"
+    echo "Expected contents to match:"
+    cat ${expected_file}
+    echo "But found contents:"
+    cat ${new_file}
+    echo "With differences"
+    diff -u ${expected_file} ${new_file}
+    echo "Test complete with failure"
+    exit 1
+  else
+    cp ${expected_file} ${prev_file}
+    rm ${new_file}
+    rm ${expected_file}
+  fi
 }
 
 comparefile=$(mktemp)
@@ -77,9 +76,9 @@ test_send_socket_string "hijklmnop" ${comparefile}
 test_send_socket_string "1234567890" ${comparefile}
 test_send_socket_string "9876543210" ${comparefile}
 if [ -e long_string.txt ]; then
-    echo "Sending long string from long_string.txt file"
-    sendstring=`cat long_string.txt`
-    test_send_socket_string ${sendstring} ${comparefile}
+  echo "Sending long string from long_string.txt file"
+  sendstring=$(cat long_string.txt)
+  test_send_socket_string ${sendstring} ${comparefile}
 fi
 echo "Full contents sent:"
 cat ${comparefile}
